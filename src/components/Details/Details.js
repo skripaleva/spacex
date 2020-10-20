f01737598e60ee10a2e0631eef35ca1c0282b587
Evgeniya Skripaleva <evgeniya_ignaten@mail.ru>
2020-10-20 17:33:43 +0300

    Day4

---
+++ src/components/Details/Details.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from "react";
+import { useHistory } from 'react-router-dom';
+import useLaunches from "../useLaunches/useLaunches";
+import Youtube from 'react-youtube';
+import './details.css';
+import Main from "../Main/Main";
+
+const Details = (props) => {
+
+    const [launch, setLaunch] = useState(null);
+
+    const { getLaunch } = useLaunches();
+
+    useEffect(() => {
+        setLaunch(getLaunch(props.match.params.id));
+    }, [getLaunch]);
+
+    const histoty = useHistory();
+
+    if (!launch) return <div>Загрузка...</div>
+
+    return (
+        <>
+            <Main name={launch.name} />
+            <main className="details">
+                <div className="container">
+                    <div className="details-row">
+                        <div className="details-image">
+                            <img src={launch.links.patch.small} alt={launch.name}/>
+                        </div>
+                        <div className="details-content">
+                            <p className="details-description">{launch.details}</p>
+                        </div>
+                    </div>
+                        <Youtube className='details-youtube' videoId={launch.links.youtube_id}/>
+                </div>
+                <a onClick={histoty.goBack} className="button button-back">go back</a>
+            </main>
+        </>
+    )
+};
+
+export default Details
