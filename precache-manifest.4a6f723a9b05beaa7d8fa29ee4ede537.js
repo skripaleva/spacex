10e7df987a7cd1affc13483cd27ffdabc923e546
Evgeniya Skripaleva <evgeniya_ignaten@mail.ru>
2020-12-17 15:32:25 +0300

    Updates

---
+++ precache-manifest.4a6f723a9b05beaa7d8fa29ee4ede537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbc89d38d3022da6b0e9f447e29531c",
+    "url": "/spacex/index.html"
+  },
+  {
+    "revision": "0495e8ffbeecc836e63e",
+    "url": "/spacex/static/css/main.858b11a4.chunk.css"
+  },
+  {
+    "revision": "9a3d7f5da8e9a0f3a711",
+    "url": "/spacex/static/js/2.3f4c1c52.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/spacex/static/js/2.3f4c1c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0495e8ffbeecc836e63e",
+    "url": "/spacex/static/js/main.6248eeca.chunk.js"
+  },
+  {
+    "revision": "38bd822f86f0fa518f7f",
+    "url": "/spacex/static/js/runtime-main.3f7de82f.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/spacex/static/media/logo.d5ab31ce.svg"
+  }
+]);
